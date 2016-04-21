b5102bf55d58cae364b915b28e2ef6746d7477a2
fox8091 <bfox200012@gmail.com>
2016-04-21 09:02:47 -0500

    Update aes.c

--- aes.c
+++ aes.c
@@ -1,38 +1,41 @@
 
 #include "types.h"
-#include "AES_REGISTERS.h"
 #include "aes.h"
 
-int set_keyslot(u8 keyslot){
+void set_keyslot(u8 keyslot){
 	if (keyslot > 0x3F && keyslot < 0) return 1;
-	SET_AES_KEYSEL(keyslot);
-	return 0;
+	AES_KEYSEL = keyslot;
+	AES_CNT |= 0x04000000;
 }
 
-int set_normalKey(u8 keyslot, void* key){
-	if (keyslot > 0x3F && keyslot < 0) return 1;
-	SET_AES_KEYCNT(keyslot);
-	SET_AES_KEYFIFO((u32*)key);
-	return 0;
+void set_normalKey(u8 keyslot, void* key){
+	if(keyslot > 0x3F && keyslot < 0) return;
+	AES_KEYCNT = keyslot;
+	for(int i = 0; i < 0x10; i += 4) AES_KEYFIFO = *(u32*)(key+i);
+	return;
 }
 
-int set_keyX(u8 keyslot, void* keyX){
-	if (keyslot > 0x3F && keyslot < 0) return 1;
-	SET_AES_KEYCNT(keyslot);
-	SET_AES_KEYXFIFO((u32*)keyX);
-	return 0;
+void set_keyX(u8 keyslot, void* keyX){
+	if(keyslot > 0x3F && keyslot < 0) return;
+	AES_KEYCNT = keyslot;
+	for(int i = 0; i < 0x10; i += 4) AES_KEYFIFO = *(u32*)(keyX+i);
+	return;
 }
 
-int set_keyY(u8 keyslot, void* keyY){
-	if (keyslot > 0x3F && keyslot < 0) return 1;
-	SET_AES_KEYCNT(keyslot);
-	SET_AES_KEYYFIFO((u32*)keyY);
-	return 0;
+void set_keyY(u8 keyslot, void* keyY){
+	if(keyslot > 0x3F && keyslot < 0) return;
+	AES_KEYCNT = keyslot;
+	for(int i = 0; i < 0x10; i += 4) AES_KEYFIFO = *(u32*)(keyY+i);
+	return;
+}
+
+void set_ctr(u32* ctr){
+	for (int i = 0; i < 4; i++) AES_CTR[i] = ctr[3-i];
 }
 
 void add_ctr(u8* ctr){
 	int i;
-	for (i = 15; i >= 0; i--){
+	for(i = 15; i >= 0; i--){
 		if (ctr[i] == 0xFF) ctr[i] = 0;
 		else {
 			ctr[i] += 1;
@@ -42,18 +45,18 @@ void add_ctr(u8* ctr){
 }
 
 void aes(void* in, void* out, void* iv, u32 blocks, u32 method){
-	SET_AES_BLKCNT(blocks<<16);
-	SET_AES_CNT(method | 0x83C00C00);
-	u32* input = in;
-	u32* output = out;
-	for (int j = 0; j < blocks*4; j+=4){
-		for (int i = 0; i < 4; i++){
-			SET_AES_WRFIFO(input[j+i]);
-			GET_AES_RDFIFO(output[j+i]);
-		}
-		if (iv != NULL){
-			add_ctr((u8*)iv);
-			SET_AES_CTR((u32*)iv);
-		}
+	AES_CNT = method | 0x03C00C00;
+
+	for(int i = 0; i < blocks * 0x10; i += 0x10){
+		AES_BLKCNT = (1 << 16);
+		if(iv != NULL) set_ctr((u32*)iv);}
+		AES_CNT |= 0x80000000
+		for(int j = 0; j < 0x10; j += 4) AES_WRFIFO = *((u32*)(in + i + j));
+		while(((AES_CNT >> 0x5) & 0x1F) < 0x4);
+		for(int j = 0; j < 0x10; j += 4) AES_RDFIFO = *((u32*)(in + i + j));
+
+		AES_CNT &= ~0x80000000;
+
+		if(method & (AES_CTR_DECRYPT | AES_CTR_ENCRYPT)) add_ctr((u8*)iv);
 	}
 }
