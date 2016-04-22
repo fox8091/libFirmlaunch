2a847ce4020c8a83512b5d010d118da14c099eee
fox8091 <bfox200012@gmail.com>
2016-04-21 23:12:14 -0500

    Update firmlaunch.h

--- firmlaunch.h
+++ firmlaunch.h
@@ -3,7 +3,7 @@
 
 #include "types.h"
 
-int firm_setup(u32* FIRM, u8 N3DSKey1, u8 N3DSKey2);
+int firm_setup(u32* FIRM, void* N3DSKey1, void* N3DSKey2);
 void firmlaunch(u32* FIRM);
 void patch(u32* FIRM);
 
