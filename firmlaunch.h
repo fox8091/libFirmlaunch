85854585eba837646ecd0977e65c95be89c53818
Bailey <bfox200012@gmail.com>
2016-04-25 18:06:53 -0500

    Fix my stupid error :/

--- firmlaunch.h
+++ firmlaunch.h
@@ -5,6 +5,6 @@
 
 int firm_setup(u32* FIRM, void* N3DSKey1, void* N3DSKey2);
 void firmlaunch(u32* FIRM);
-int patch(u32* FIRM, u32* search_size, u8* pattern, u8* patch_data, u32 pattern_size, u32 patch_size);
+int patch(u32* FIRM, u32 search_size, u8* pattern, u8* patch_data, u32 pattern_size, u32 patch_size);
 
 #endif
