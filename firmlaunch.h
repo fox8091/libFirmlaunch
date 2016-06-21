44550fce03d0cd31c3323622f4e15ee3dcca0254
Bailey <bfox200012@gmail.com>
2016-06-20 20:21:02 -0500

    Expose pattern_match

--- firmlaunch.h
+++ firmlaunch.h
@@ -4,4 +4,5 @@
 
 int firm_setup(u32* FIRM, void* N3DSKey1, void* N3DSKey2);
 void firmlaunch(u32* FIRM);
+void *pattern_match(u8* baseaddr, u32 search_size, u8* pattern, u32 size, bool reversed)
 int patch(u32* FIRM, u32 search_size, u8* pattern, u8* patch_data, u32 pattern_size, u32 patch_size, u32 offset, bool reversed);
